e09b467b001253ae1904649896d8423c73812d58
badpokhs <badpokhs@gmail.com>
2023-04-19 11:34:10 +0545

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+  const [count, setCount] = useState(0);
+  const handleIncreaseCount = () => {
+    setCount(count + 1);
+  }; 
+  const handleDecreaseCount = () => {
+    setCount(count -1);
+  };
+  const handleResetCount = () => {
+    setCount(0);
+  };
 
+return(
+  <div>
+    <h2>Counter App</h2>
+    <h1>{count}</h1>
+    <table>
+      <thead>
+        <tr>
+          <th><button onClick={handleDecreaseCount}> Decrease </button></th>
+          <th><button onClick={handleResetCount}> Reset </button></th>
+          <th><button onClick={handleIncreaseCount}> Increase </button></th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+)
+};
 export default App;
